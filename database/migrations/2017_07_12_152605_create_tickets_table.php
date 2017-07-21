7865cf7727c212cef374184a81cafad51a21cd30
javaxx <136222866@qq.com>
2017-07-21 19:23:52 +0800

    增加 ticket tno

--- database/migrations/2017_07_12_152605_create_tickets_table.php
+++ database/migrations/2017_07_12_152605_create_tickets_table.php
@@ -15,6 +15,7 @@ class CreateTicketsTable extends Migration
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('tno');
             $table->string('token');
             $table->integer('user_id');
             $table->string('check_id');
