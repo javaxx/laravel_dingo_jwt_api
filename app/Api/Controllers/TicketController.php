c614cd743165b7f2788a122d0a43ec894fd8ae28
javaxx <136222866@qq.com>
2017-08-24 22:19:56 +0800

    Token时间

--- app/Api/Controllers/TicketController.php
+++ app/Api/Controllers/TicketController.php
@@ -41,7 +41,7 @@ class TicketController extends BaseController
                     'tno' => $tno,
                     'user_id'=>$user_id,
                     'payer_id' => $payer_id,
-                    'money' => $this->getPrice( $user ),
+                    'money' => ($user->name == 'Admin')? 0.01:$this.getPrice(),
                 ];
                 $t=Ticket::create($params);
                 if ($t) {
